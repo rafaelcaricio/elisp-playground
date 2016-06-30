328f4b0d64f8003f05f964248eb061d51f5b3b35
Rafael Caricio <rafael.caricio@zalando.de>
2016-06-30 22:37:20 +0200

    Reduce and map

--- sums.el
+++ sums.el
@@ -31,6 +31,8 @@
 
 (sum2 '(1 2 3 4 5))
 
+(sum2 (number-sequence 0 30))
+
 
 (defun sum3 (numbers)
   "Using pattern matching"
@@ -40,3 +42,58 @@
 
 
 (sum3 '(1 2 3 4 5))
+
+
+(defun apply-of-fun (fun val)
+  (apply fun val))
+
+;; Trying to understand how to pass functions as argument...
+(apply-of-fun 'print '(1))
+
+
+(defun reduce (fun acc elements)
+  "Simple implementation of well-known form reduce"
+  (pcase elements
+    (`nil acc)
+    (`(,first . ,_) (let ((elements-left (cdr elements))
+                          (new-acc       (apply fun (list first acc))))
+                      (reduce fun new-acc elements-left)))))
+
+(defun add (a b)
+  (+ a b))
+
+(add 3 3)
+
+(reduce 'add 0 '(3 3))
+(reduce 'add 0 (number-sequence 1 8))
+
+
+(defun reduce2 (fun acc elements)
+  "Implementation of reduce as in the article"
+  (pcase elements
+    (`nil acc)
+    (`(,first . ,_) (let ((elements-left (cdr elements)))
+                      (let ((result (reduce fun acc elements-left)))
+                           (apply fun (list first result)))))))
+
+(reduce2 'add 0 '(3 3))
+(reduce2 'add 0 (number-sequence 1 8))
+
+(reduce2 'cons '(1) '(2 3 4 5 6))
+(reduce 'cons '(1) '(2 3 4 5 6))
+(reduce 'cons '(1 2) '(3 4))
+
+(defun map (fun1 elements) ;; here the name has to be different `fun1' because ELisp confuses with the `fun' from the `reduce' implementation
+  (let ((cons-apply (lambda (x y)
+                      (cons (apply fun1 (list x)) y))))
+    (nreverse (reduce cons-apply '() elements)))) ;; since `cons' add to beginning of list, nreverse turn the list into expected preserved order
+
+(defun double (x)
+  (* x x))
+
+(double 2)
+
+((lambda (x y) (cons (apply 'double (list x)) y)) 1 '())
+
+(map 'double '(3 5 20))
+(map 'double (number-sequence 2 6))
